5fecd7947ca6681f2ff72e8545ef0c1ad020be6a
Esteban0104 <esgascar@gmail.com>
2022-01-19 06:44:22 -0500

    Agregue las rutas exitosamente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductsComponent} from './products/products.component';
+import {ContactsComponent} from './contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',
+component: HomeComponent
+  },
+  {
+    path: 'products',
+  component: ProductsComponent
+    },
+    {
+      path: 'contacts',
+    component: ContactsComponent
+      }
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
