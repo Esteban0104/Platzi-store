68a774f7b0207be35a03d1cb7155051251399d4b
Esteban0104 <esgascar@gmail.com>
2022-02-02 11:31:10 -0500

    Navbar y footer hechos

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
-
+    MatButtonModule,
+    MatToolbarModule
   ],
   exports: [
-    MatButtonModule
-
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatCardModule
+    
   ]
 })
 export class MaterialModule { }
