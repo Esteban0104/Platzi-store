e60428d0ad1bf38af65c1a594ea716bb1a8154ea
Esteban0104 <esgascar@gmail.com>
2022-02-07 03:54:08 -0500

    Pagina de contacto 90% terminada

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,4 +13,5 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
+  
 }
